47b2482239c739e4ee0b457d00ebcb2d45c47cfb
clashGM01 <clashoflegends.thegame@gmail.com>
2019-11-02 19:12:41 -0600

    sort properties before saving

--- src/persistenceCommons/SysProperties.java
+++ src/persistenceCommons/SysProperties.java
@@ -15,8 +15,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Vector;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -27,7 +35,7 @@ import org.apache.commons.logging.LogFactory;
 final class SysProperties implements Serializable {
 
     private static final Log log = LogFactory.getLog(SysProperties.class);
-    private Properties props;
+    private MyProperties props;
     private final String propArqName = "properties.config";
     private final String comentario = "Counselor config file\n"
             + "filtro.default=0|1 -> All|Own\n"
@@ -80,7 +88,7 @@ final class SysProperties implements Serializable {
      * Creates a new instance of SysProperties
      */
     protected SysProperties() {
-        props = new Properties();
+        props = getPropertiesNew();
         doConfigRestore();
     }
 
@@ -122,8 +130,8 @@ final class SysProperties implements Serializable {
         }
     }
 
-    protected Properties doLoadPropertiesFile(String fileName) throws IOException {
-        Properties properties = new Properties();
+    protected MyProperties doLoadPropertiesFile(String fileName) throws IOException {
+        MyProperties properties = new MyProperties();
         InputStream propIn = new FileInputStream(new File(fileName));
         properties.load(propIn);
         return properties;
@@ -161,6 +169,69 @@ final class SysProperties implements Serializable {
         } catch (IOException e) {
             log.error("Error while creating new config file.");
             return false;
+        }
+    }
+
+    private MyProperties getPropertiesNew() {
+        return new MyProperties() {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Set<Object> keySet() {
+                return Collections.unmodifiableSet(new TreeSet<Object>(super.keySet()));
+            }
+
+            @Override
+            public Set<Map.Entry<Object, Object>> entrySet() {
+
+                Set<Map.Entry<Object, Object>> set1 = super.entrySet();
+                Set<Map.Entry<Object, Object>> set2 = new LinkedHashSet<Map.Entry<Object, Object>>(set1.size());
+
+                Iterator<Map.Entry<Object, Object>> iterator = set1.stream().sorted(new Comparator<Map.Entry<Object, Object>>() {
+
+                    @Override
+                    public int compare(java.util.Map.Entry<Object, Object> o1, java.util.Map.Entry<Object, Object> o2) {
+                        return o1.getKey().toString().compareTo(o2.getKey().toString());
+                    }
+                }).iterator();
+
+                while (iterator.hasNext()) {
+                    set2.add(iterator.next());
+                }
+
+                return set2;
+            }
+
+            @Override
+            public synchronized Enumeration<Object> keys() {
+                return Collections.enumeration(new TreeSet<Object>(super.keySet()));
+            }
+        };
+    }
+
+    public static class MyProperties extends Properties {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public Enumeration<Object> keys() {
+            Enumeration<Object> keysEnum = super.keys();
+            Vector<Object> keyList = new Vector<Object>();
+
+            while (keysEnum.hasMoreElements()) {
+                keyList.add(keysEnum.nextElement());
+            }
+
+            Collections.sort(keyList, new Comparator<Object>() {
+                @Override
+                public int compare(Object o1, Object o2) {
+                    return o1.toString().compareTo(o2.toString());
+                }
+            });
+            System.out.println("persistenceCommons.SysProperties.MyProperties.keys()");
+
+            return keyList.elements();
         }
     }
 
