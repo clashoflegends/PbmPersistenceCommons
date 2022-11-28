66994eb3717fcdb79beb9dd72e15b7e988cdb902
clashGM01 <clashoflegends.thegame@gmail.com>
2022-11-28 12:51:01 -0700

    Add debug flag to HTTP post duration

--- src/persistenceCommons/WebCounselorManager.java
+++ src/persistenceCommons/WebCounselorManager.java
@@ -28,7 +28,7 @@ import org.apache.http.util.EntityUtils;
  * @author jmoura
  */
 public class WebCounselorManager {
-    
+
     private static final Log log = LogFactory.getLog(WebCounselorManager.class);
     private static WebCounselorManager instance;
     private static final String siteUrl = "http://clashlegends.com/PbmSite/%s.php";
@@ -39,10 +39,10 @@ public class WebCounselorManager {
     public static final int ERROR_UNKOWN = 0;
     private int lastStatusCode;
     private String lastResponseString;
-    
+
     private WebCounselorManager() {
     }
-    
+
     public synchronized static WebCounselorManager getInstance() {
         if (instance == null) {
             log.debug("Criou instancia do WebManager.");
@@ -55,7 +55,7 @@ public class WebCounselorManager {
     public int doSendViaPost(PartidaJogadorWebInfo info) throws PersistenceException {
         try {
             HttpClient client = new DefaultHttpClient();
-            
+
             MultipartEntity entity = new MultipartEntity();
             // o primeiro parametro eh o nome do "campo" onde se espera enviar o arquivo. Deve
             // ser igual ao determinado no PHP. O segundo eh o arquivo local
@@ -82,6 +82,8 @@ public class WebCounselorManager {
 
             // aqui define a URL
             final String uploadPage = SettingsManager.getInstance().getConfig("UploadPage", "CounselorUploadTurn");
+            long start = System.currentTimeMillis();
+
             HttpPost post = new HttpPost(getUrl(uploadPage));
             post.setEntity(entity);
 
@@ -89,6 +91,11 @@ public class WebCounselorManager {
             log.debug("Executing request: " + post.getRequestLine());
             //String response = client.execute(post, new BasicResponseHandler());
             HttpResponse response = client.execute(post);
+            long end = System.currentTimeMillis();
+            if (SettingsManager.getInstance().isConfig("DebugWebpostTime", "1", "0")) {
+                log.info("Round trip response time from server = " + (end - start) + " millis");
+            }
+
             setLastStatusCode(response.getStatusLine().getStatusCode());
             setLastResponseString(responseToString(response));
             //process responses
@@ -116,31 +123,31 @@ public class WebCounselorManager {
         }
         return ERROR_UNKOWN;
     }
-    
+
     private URI getUrl(String webpage) throws URISyntaxException {
         return new URI(String.format(siteUrl, webpage));
     }
-    
+
     private String responseToString(HttpResponse response) throws ParseException, IOException {
         return EntityUtils.toString(response.getEntity());
     }
-    
+
     public int getLastStatusCode() {
         return lastStatusCode;
     }
-    
+
     private void setLastStatusCode(int lastStatusCode) {
         this.lastStatusCode = lastStatusCode;
     }
-    
+
     public String getLastResponseString() {
         return lastResponseString;
     }
-    
+
     private void setLastResponseString(String lastResponseString) {
         this.lastResponseString = lastResponseString;
     }
-    
+
     private String getbooleanStatus(boolean portrait) {
         if (portrait) {
             return "ON";
